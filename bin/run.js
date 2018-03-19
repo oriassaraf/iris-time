f46d89240290ee1d7d734b66dc02362b3c1108ca
oriassaraf <ori.assaraf@gmail.com>
2018-03-19 13:23:46 +0200

    Removeing toekn to .gitignore's app-env file (localy only)

--- bin/run.js
+++ bin/run.js
@@ -5,17 +5,17 @@ const service = require('../server/service');
 const http = require('http');
 const server = http.createServer(service);
 
-const witToken = 'IIDSS2XM2546IZZNU4OS6526FXTJM3PA';
+const witToken = process.env.WIT_TOKEN;
 const witClient = require('../server/witClient')(witToken);
 
-const slackToken = 'xoxb-329788960100-txjC8QpU6KzdMoFzLfgg09aB';
+const slackToken = process.env.SLACK_TOKEN;
 const slackLogLevel = 'verbose';
 
 const rtm = slackClient.init(slackToken, slackLogLevel, witClient);
 rtm.start();
 
-slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
+slackClient.addAuthenticatedHandler(rtm, () => server.listen(3010));
 
 server.on('listening', function () {
-    console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
+    console.log(`IRIS-TIME is listening on ${server.address().port} in ${service.get('env')} mode.`);
 });
--- server/service.js
+++ server/service.js
@@ -3,4 +3,8 @@
 const express = require ('express');
 const service = express();
 
+service.get('/service/:location', (req, res, next) => {
+    res.json({result: req.params.location});
+});
+
 module.exports = service;
