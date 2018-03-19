24c35e24c38084baf9843cd00d4fc75a723ae873
oriassaraf <ori.assaraf@gmail.com>
2018-03-19 18:29:31 +0200

    Add TIMEZON API call to convert lat/lng to timezone

--- server/service.js
+++ server/service.js
@@ -3,6 +3,10 @@
 const express = require ('express');
 const service = express();
 const request = require('superagent');
+const moment = require('moment');
+
+//https://maps.googleapis.com/maps/api/timezone/json?location=38.908133,-77.047119&timestamp=1458000000&key=YOUR_API_KEY
+//AIzaSyCIY_7PuloxDh-6HR1UjkK4_pwzNm1w2dE
 
 service.get('/service/:location', (req, res, next) => {
     request.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + req.params.location + '&key=' + process.env.GOOGLE_GEOCODING_API_TOKEN, (err, response) => {
@@ -10,7 +14,21 @@ service.get('/service/:location', (req, res, next) => {
             console.log(err);
             return res.sendStatus(500);
         }
-        res.json(response.body.results[0].geometry.location);
+        
+        const location = response.body.results[0].geometry.location;
+        const timestamp = +moment().format('X');
+
+        request.get('https://maps.googleapis.com/maps/api/timezone/json?location=' + location.lat + ',' + location.lng + '&timestamp=' + timestamp + '&key=' + process.env.GOOGLE_TINEZONE_API_TOKEN, (err, response) => {
+            if (err) {
+                console.log(err);
+                return res.sendStatus(500);
+            }
+
+            const result = response.body;
+            const timeString = moment.unix(timestamp + result.dstOffset + result.rawOffset).utc().format('dddd, MMMM Do YYYY, h:mm:ss a');
+
+            res.json({results: timeString});
+        });
     });
 });
 
