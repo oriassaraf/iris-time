2c59708ad76cde2a2d0571bad90d2d4632cbf7b2
oriassaraf <ori.assaraf@gmail.com>
2018-03-26 17:28:58 +0300

    Announce service

--- bin/run.js
+++ bin/run.js
@@ -1,11 +1,25 @@
 'use strict';
-
+const request = require('superagent');
 const service = require('../server/service');
 const http = require('http');
-const server = http.createServer(service);
 
-server.listen(3010);
+const server = http.createServer(service);
+server.listen();
 
 server.on('listening', function () {
     console.log(`IRIS-TIME is listening on ${server.address().port} in ${service.get('env')} mode.`);
+
+    const announce = () => {
+        request.put(`http://127.0.0.1:3000/service/time/${server.address().port}`, (err, res) => {
+            if (err) {
+                console.log(err);
+                console.log("Error connecting to IRIS");
+                return;
+            }
+            console.log(res.body);
+        });
+    };
+
+    announce();
+    setInterval(announce, 15*1000);
 });
